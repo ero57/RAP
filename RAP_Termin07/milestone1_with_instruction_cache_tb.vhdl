9416c28f8d837db2716b3252684016c713c48035
ero57 <91624399+ero57@users.noreply.github.com>
2023-06-20 15:06:37 +0200

    Update milestone1_with_instruction_cache_tb.vhdl

--- RAP_Termin07/milestone1_with_instruction_cache_tb.vhdl
+++ RAP_Termin07/milestone1_with_instruction_cache_tb.vhdl
@@ -6,47 +6,46 @@ entity milestone1_with_instruction_cache_tb is end entity;
 
 architecture behav of milestone1_with_instruction_cache_tb is
 
-  -- Konvertiert ein std_logic_vector zu einem String
+  -- Funktion zur Umwandlung von std_logic_vector zu string
   function to_string(arg : std_logic_vector) return string is
 	  variable result : string (1 to arg'length);
 	  variable v : std_logic_vector (result'range) := arg;
-	  begin
-	    for i in result'range loop
-		    case v(i) is
-			    when 'U' => result(i) := 'U';
-			    when 'X' => result(i) := 'X';
-			    when '0' => result(i) := '0';
-			    when '1' => result(i) := '1';
-			    when 'Z' => result(i) := 'Z';
-			    when 'W' => result(i) := 'W';
-			    when 'L' => result(i) := 'L';
-			    when 'H' => result(i) := 'H';
-			    when '-' => result(i) := '-';
-			    end case;
-	    end loop;
-	    return result;
+  begin
+	  for i in result'range loop
+		  case v(i) is
+			  when 'U' => result(i) := 'U';
+			  when 'X' => result(i) := 'X';
+			  when '0' => result(i) := '0';
+			  when '1' => result(i) := '1';
+			  when 'Z' => result(i) := 'Z';
+			  when 'W' => result(i) := 'W';
+			  when 'L' => result(i) := 'L';
+			  when 'H' => result(i) := 'H';
+			  when '-' => result(i) := '-';
+			  end case;
+	  end loop;
+	  return result;
   end function;
 
-  -- Deklaration der CPU Komponente
+  -- Deklaration der Komponente 'milestone1'
   component milestone1 is port (
     cpuclk : in std_logic;
+
     debug_rd : out std_logic_vector(31 downto 0);
     debug_addr_of_rd : out std_logic_vector(4 downto 0);
     debug_mem_ready : out std_logic
   ); end component;
 
-  -- Signal Deklarationen
+  -- Deklaration der Signale
   signal cpuclk : std_logic;
   signal debug_rd : std_logic_vector(31 downto 0);
   signal debug_addr_of_rd : std_logic_vector(4 downto 0);
   signal debug_mem_ready : std_logic;
 
-  -- Testdaten
+  -- Erwartete Lesedaten
   type exrd is array (0 to 255) of std_logic_vector(31 downto 0);
   signal expectrd : exrd := (
-                   -- main:
     x"00000123",   --   addi    x1,zero,0x123
-                   -- loop:
     x"00108093",   --   addi    x1,x1,1
     x"ffdfffef",   --   jal     x31,loop
 
@@ -56,34 +55,64 @@ begin
   -- Instanziierung der CPU
   my_cpu: milestone1  port map(cpuclk, debug_rd, debug_addr_of_rd, debug_mem_ready);
 
-  -- Haupttestprozess
   process
-    variable count : integer := 10000; -- ändern Sie die Anzahl der Taktimpulse auf 10000
-    variable miss4 : integer := 0;
+    variable count : integer := 10000;  -- 10000 Taktzyklen
+    variable cnt8 : integer := -1;
+    variable x1val : integer := 256+32+3;
+    variable pcval : integer := 0;
+    variable miss4 : integer := 0;  -- Zählt die Anzahl der Cache-Misses
   begin
     cpuclk <= '0'; wait for 1 fs;
 
     -- Haupttestschleife
-    while count > 0 loop
+    while true loop
       count := count - 1;
-      
+      if count=0 then
+        report "Test end forced after 10000 cpu clks";
+        report "cache misses for address 4: " & integer'image(miss4);
+        if miss4 > 20 then  -- Wenn mehr als 20 Cache-Misses, Test ist fehlgeschlagen
+          report "That's too much. Test failed." severity failure;
+        end if;
+        report "That's ok --- Test success";
+        wait;
+      end if;
       cpuclk <= '1'; wait for 1 fs; cpuclk <= '0'; wait for 1 fs; 
 
-      -- Wenn debug_mem_ready = '0' und debug_addr_of_rd = "00001" (Adresse x1), dann erhöhen Sie miss4
-      if debug_mem_ready = '0' and debug_addr_of_rd = "00001" then
-        miss4 := miss4 + 1;
+      -- Überprüfung auf Cache-Miss
+      if debug_mem_ready='0' then
+        if pcval=4 then
+          miss4 := miss4 + 1;
+        end if;
       end if;
-      
-      -- Test beenden, wenn count = 0
-      if count = 0 then
-	report "Test end forced after 10000 cpu clks";
-        report "cache misses for address 4: " & integer'image(miss4);
-        -- Überprüfen, ob die Anzahl der Cache-Misses unter 20 liegt
-        if miss4 > 20 then
-          report "That's too much. Test failed." severity failure;
+
+      -- Synchronisation und Prüfung der erwarteten Werte
+      if cnt8<0  and debug_addr_of_rd/="00000" then
+        report "sync!";
+        cnt8:=8;
+      elsif cnt8>=0 then
+        cnt8:=cnt8-1;
+        if cnt8=0 then
+          cnt8:=8;
+        end if;
+      end if;
+      if cnt8=8 then
+        if pcval=0  or  pcval=4  then
+          if debug_addr_of_rd/="00001"   or  to_integer(unsigned(debug_rd))/=x1val then
+            report "expected: addr_of_rd=00001 rd=" & integer'image(x1val) severity failure;
+          end if;
+          x1val := x1val + 1;
+          pcval := pcval + 4;
+        elsif pcval=8 then
+          if debug_addr_of_rd/="11111"   or  debug_rd/=x"0000000c" then
+            report "expected: addr_of_rd=00001 rd=" & integer'image(x1val) severity failure;
+          end if;
+          pcval := 4;
         else
-	    report "That's ok --- Test success";
-	    wait;
+          report "pcval ist weggelaufen ..." severity failure;
+        end if;
+      elsif cnt8>=0 then
+        if debug_rd/=x"00000000"  or  debug_addr_of_rd/="00000" then
+          report "expected nop" severity failure;
         end if;
       end if;
     end loop;
