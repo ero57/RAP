aec967286882a47bd90b4391d031ae9ad9d9b547
ero57 <91624399+ero57@users.noreply.github.com>
2024-03-06 15:20:01 +0100

    Add files via upload

--- RAP_Termin07/instruction_cache_tb.vhdl
+++ RAP_Termin07/instruction_cache_tb.vhdl
@@ -25,69 +25,51 @@ architecture behav of instruction_cache_tb is
     x"00108093",   --   addi	x1,x1,1
     x"ffdfffef",   --   jal	x31,loop
   others=>x"00000000");
-  signal from_rom : std_logic_vector(31 downto 0);
 
 begin
   my_cache: instruction_cache port map(pc_value, ir_2_cpu, cpuclk, cachehit);
 
-  -- from_rom <= rom(to_integer(unsigned(pc_value(9 downto 2))));
-
   process
-    variable temp : integer;
   begin
-    report "---- reading address 0x00000004";
-    pc_value <= x"00000004"; wait for 1 fs;
-    for i in 1 to 5 loop
-      if cachehit='1' then
-        report "Cache responds with cachehit too fast, after " & integer'image(i) & " clocks" severity failure;
-      end if;
-      cpuclk <= '1'; wait for 1 fs; cpuclk <= '0'; wait for 1 fs;
-    end loop;
-    if cachehit/='1'  or  ir_2_cpu/=x"00108093" then
-      temp := 0; if cachehit='1' then temp := 1; end if;
-      report "cachehit=" & integer'image(temp) & ", ir_2_cpu=" & integer'image(to_integer(unsigned(ir_2_cpu)));
-      report "Cache responds with cachehit too late and/or wrong data" severity failure;
-    end if;
-
-    report "---- reading address 0x00000014";
-    pc_value <= x"00000014"; wait for 1 fs;
-    for i in 1 to 5 loop
-      if cachehit='1' then
-        report "Cache responds with cachehit too fast, after " & integer'image(i) & " clocks" severity failure;
-      end if;
-      cpuclk <= '1'; wait for 1 fs; cpuclk <= '0'; wait for 1 fs;
-    end loop;
-    if cachehit/='1'  or  ir_2_cpu/=x"00000000" then
-      temp := 0; if cachehit='1' then temp := 1; end if;
-      report "cachehit=" & integer'image(temp) & ", ir_2_cpu=" & integer'image(to_integer(unsigned(ir_2_cpu)));
-      report "Cache responds with cachehit too late and/or wrong data" severity failure;
-    end if;
-
-    report "---- reading address 0x00000004 again";
-    pc_value <= x"00000004"; wait for 1 fs;
-    if cachehit/='1'  or  ir_2_cpu/=x"00108093" then
-      report "Cache responds with cachehit too late and/or wrong data for address 4" severity failure;
-    end if;
-
-    report "---- reading address 0x00000008";
-    pc_value <= x"00000008"; wait for 1 fs;
-    for i in 1 to 5 loop
-      if cachehit='1' then
-        report "Cache responds with cachehit too fast, after " & integer'image(i) & " clocks" severity failure;
-      end if;
-      cpuclk <= '1'; wait for 1 fs; cpuclk <= '0'; wait for 1 fs;
-    end loop;
-    if cachehit/='1'  or  ir_2_cpu/=x"ffdfffef" then
-      temp := 0; if cachehit='1' then temp := 1; end if;
-      report "cachehit=" & integer'image(temp) & ", ir_2_cpu=" & integer'image(to_integer(unsigned(ir_2_cpu)));
-      report "Cache responds with cachehit too late and/or wrong data" severity failure;
-    end if;
-
-    report "---- reading address 0x00000004 again";
-    pc_value <= x"00000004"; wait for 1 fs;
-    if cachehit/='1'  or  ir_2_cpu/=x"00108093" then
-      report "Cache responds with cachehit too late and/or wrong data for address 4" severity failure;
-    end if;
+  -- Addresse an x"00000000" lesen, befehl  addi	x1,zero,0x123)
+  pc_value <= x"00000000";
+  wait for 1 fs;
+  for i in 0 to 4 loop
+  	if cachehit = '1' then report "Cache reagiert zu früh" severity failure;
+  	end if;
+  	cpuclk <= '1'; wait for 1 fs; cpuclk <= '0'; wait for 1 fs; cpuclk <= '1'; wait for 1 fs;
+  end loop; 
+  if cachehit /= '1' then report "Cache reagiert zu spät" severity failure;
+  end if;
+  if ir_2_cpu /= x"12300093" then report "Falsche Daten";
+  end if;
+  
+    -- Addresse an x"00000004" lesen, befehl addi	x1,x1,1)
+  pc_value <= x"00000004";
+  wait for 1 fs;
+  for i in 0 to 4 loop
+  	if cachehit = '1' then report "Cache reagiert zu früh" severity failure;
+  	end if;
+  	cpuclk <= '1'; wait for 1 fs; cpuclk <= '0'; wait for 1 fs; cpuclk <= '1'; wait for 1 fs;
+  end loop; 
+  if cachehit /= '1' then report "Cache reagiert zu spät" severity failure;
+  end if;
+  if ir_2_cpu /= x"00108093" then report "Falsche Daten";
+  end if;
+  
+  -- Addresse an x"00000008" lesen, befehl jal	x31,loop)
+  pc_value <= x"00000008";
+  wait for 1 fs;
+  for i in 0 to 4 loop
+  	if cachehit = '1' then report "Cache reagiert zu früh" severity failure;
+  	end if;
+  	cpuclk <= '1'; wait for 1 fs; cpuclk <= '0'; wait for 1 fs; cpuclk <= '1'; wait for 1 fs;
+  end loop; 
+  if cachehit /= '1' then report "Cache reagiert zu spät" severity failure;
+  end if;
+  if ir_2_cpu /= x"ffdfffef" then report "Falsche Daten";
+  end if;
+    -- report "cache test has no tests yet --- failure" severity failure;
 
     report "cache test end --- looks ok";
     wait;
